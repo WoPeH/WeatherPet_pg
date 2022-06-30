e666b3b05b2dd094cd694407c642f61b43016fd6
Stsiapan Altukhou <styopa.by@gmail.com>
2022-06-30 19:25:14 +0300

    Add another lib, Parse response, demo output

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,15 @@
 package com.company;
 
-
+import org.json.*;
 import java.io.IOException;
 
 class Main {
     public static void main(String[] args) throws IOException {
 
         WeatherAPI getResponseFromAPI = new WeatherAPI();
-        getResponseFromAPI.getCurrentWeatherResponse();
-        getResponseFromAPI.getWeatherForecastResponse();
-
+//        getResponseFromAPI.getWeatherForecastResponse();
+        //getResponseFromAPI.getCurrentWeatherResponse(); - string
+        JSONObject currentMinskState = new JSONObject(getResponseFromAPI.getCurrentWeatherResponse());
+        System.out.println("Сейчас " + (currentMinskState.getJSONObject("main").getDouble("temp")-273.15) + " градусов тепла по Цельсию");
     }
 }
