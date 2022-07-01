6d621916875bed71986fcd8c37274773308b6c96
Stsiapan Altukhou <styopa.by@gmail.com>
2022-07-01 23:04:11 +0300

    add feelsLike temperature, add comments in Main()

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,23 +7,24 @@ import java.io.IOException;
 
 class Main {
     public static void main(String[] args) throws IOException {
-        WeatherAPI getResponseFromAPI = new WeatherAPI();
-        double temp;
-        String weatherDescription;
+        WeatherAPI getResponseFromAPI = new WeatherAPI(); //Initialize API to perform requests and get response as string
+        double temp; //temperature variable
+        double tempFeelsLike;
+        String weatherDescription; //string for weather description
 //        getResponseFromAPI.getWeatherForecastResponse(); - TBD Forecast
 //        getResponseFromAPI.getCurrentWeatherResponse(); - string
-        try {
-            JSONObject currentMinskStateJSON = new JSONObject(getResponseFromAPI.getCurrentWeatherResponse());
-            WeatherStateParser stateParser = new WeatherStateParser();
-            temp = stateParser.getCurrentTemp(currentMinskStateJSON);
-            System.out.println("Сейчас " + temp + " градусов по Цельсию");
-            weatherDescription = stateParser.getCurrentWeatherDescription(currentMinskStateJSON);
-            System.out.println("Погода сейчас:\n" + weatherDescription);
+        try { //try-catch to throw exception in case json is invalid
+            JSONObject currentMinskStateJSON = new JSONObject(getResponseFromAPI.getCurrentWeatherResponse()); //Initialize JSONObject to store response
+            WeatherStateParser stateParser = new WeatherStateParser(); //Initialize Parser to get values from response
+            temp = stateParser.getCurrentTemp(currentMinskStateJSON); //get current temperature from JSON
+            System.out.println("\nСейчас " + Math.round(temp) + "° Цельсия"); //output current temperature
+            tempFeelsLike = stateParser.getCurrentFeelsLikeTemp(currentMinskStateJSON);
+            System.out.println("Ощущается как " + Math.round(tempFeelsLike) + "° Цельсия");
+            weatherDescription = stateParser.getCurrentWeatherDescription(currentMinskStateJSON); //get weather description from JSON, map it to local naming
+            System.out.println("Погода сейчас:\n" + weatherDescription); //output weather description
         } catch (JSONException exception) {
             System.out.println("Invalid JSON");
             System.exit(1);
         }
-
-//        System.out.println("Сейчас " + (currentMinskState.getJSONObject("main").getDouble("temp")-273.15) + " градусов тепла по Цельсию");
     }
 }
--- src/com/company/WeatherStateParser.java
+++ src/com/company/WeatherStateParser.java
@@ -8,6 +8,10 @@ public class WeatherStateParser {
         return weatherResponse.getJSONObject("main").getDouble("temp") - 273.15;
     }
 
+    public double getCurrentFeelsLikeTemp(JSONObject weatherResponse) throws JSONException {
+        return weatherResponse.getJSONObject("main").getDouble("feels_like") - 273.15;
+    }
+
     public String getCurrentWeatherDescription(JSONObject weatherResponse) throws JSONException {
         return WeatherDescriptionMapper.descriptionMapper(weatherResponse.getJSONArray("weather").getJSONObject(0).getInt("id"));
     }
